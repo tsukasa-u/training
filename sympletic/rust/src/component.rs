2e05864d82796f7a3853fa8dca81c0777ec6a308
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-13 13:00:35 +0900

    Create component.rs
    include mod

---
+++ sympletic/rust/src/component.rs
@@ -0,0 +1,10 @@
+#[allow(non_snake_case)]
+pub mod Julian;
+#[allow(non_snake_case)]
+pub mod Vec3;
+#[allow(non_snake_case)]
+pub mod VecN;
+#[allow(non_snake_case)]
+pub mod VecL;
+pub mod moons;
+pub mod rk4_fehlberg;
