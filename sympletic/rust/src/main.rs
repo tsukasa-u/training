c821f2a6febe2656534c1ce81156a4f551d0fe0e
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-15 21:17:06 +0900

    Update main.rs

--- sympletic/rust/src/main.rs
+++ sympletic/rust/src/main.rs
@@ -2,7 +2,7 @@
 mod component;
 
 #[allow(unused_imports)]
-use component::{Vec3, VecL, Julian};
+use component::{Vec3, VecL, Julian, PhyQty};
 
 #[allow(non_snake_case)]
 mod Moon;
@@ -18,29 +18,21 @@ static INSTANCE_D: once_cell::sync::OnceCell<Vec<f64>> =  once_cell::sync::OnceC
 #[allow(dead_code)]
 const PHY_G:f64 = 9.8;
 
-struct Phymqp {
-    name: String,
-    m: f64,
-    r: Vec3::Vec3<f64>,
-    v: Vec3::Vec3<f64>,
-    #[allow(unused)]
-    rudius: f64
-}
 
 #[allow(dead_code)]
-fn record_orbit(_t: f64, interval:f64, satelite: &mut Phymqp, _obj: &mut Vec<Phymqp>, recoder: &mut Vec<[f32; 10]>) {
+fn record_orbit(_t: f64, interval:f64, satelite: &mut PhyQty::mxvr, _obj: &mut Vec<PhyQty::mxvr>, recoder: &mut Vec<[f32; 10]>) {
     if _t%interval < 1.0 {
         let mut ret:[f32;10] = [0.0; 10];
         let mut iter: usize = 4;
         ret[0] = _t as f32;
-        ret[1] = satelite.r.get()[0] as f32;
-        ret[2] = satelite.r.get()[1] as f32;
-        ret[3] = satelite.r.get()[2] as f32;
+        ret[1] = satelite.get_x()[0] as f32;
+        ret[2] = satelite.get_x()[1] as f32;
+        ret[3] = satelite.get_x()[2] as f32;
         for v in _obj {
-            if v.name.contains("EARTH") == false {
-                ret[iter    ] = v.r.get()[0] as f32;
-                ret[iter + 1] = v.r.get()[1] as f32;
-                ret[iter + 2] = v.r.get()[2] as f32;
+            if v.get_name().contains("EARTH") == false {
+                ret[iter    ] = v.get_x()[0] as f32;
+                ret[iter + 1] = v.get_x()[1] as f32;
+                ret[iter + 2] = v.get_x()[2] as f32;
                 iter += 3;
             }
         }
@@ -49,16 +41,18 @@ fn record_orbit(_t: f64, interval:f64, satelite: &mut Phymqp, _obj: &mut Vec<Phy
 }
 
 #[allow(dead_code)]
-fn update_orbit(_t: f64, _obj:&mut Vec<Phymqp>) {
+fn update_orbit(_t: f64, _obj:&mut Vec<PhyQty::mxvr>) {
     for key in _obj {
-        match key.name.as_str() {
+        match (*key).get_name().as_str() {
             "MOON" => {
-                cspice::call_splpos_c_moon_earth_j2000((_t) as f64, key.r.as_mut_ptr());
-                key.r *= 1000.0;
+                let x = key.get_x_ptr();
+                cspice::call_splpos_c_moon_earth_j2000((_t) as f64, (*x).as_mut_ptr());
+                *x *= 1000.0;
             },
             "SUN"  => {
-                cspice::call_splpos_c_sun_earth_j2000(_t as f64, key.r.as_mut_ptr());
-                key.r *= 1000.0;
+                let x = key.get_x_ptr();
+                cspice::call_splpos_c_sun_earth_j2000(_t as f64, (*x).as_mut_ptr());
+                *x *= 1000.0;
             },
             _      => {}
         }
@@ -67,24 +61,26 @@ fn update_orbit(_t: f64, _obj:&mut Vec<Phymqp>) {
 
 #[allow(dead_code)]
 #[allow(non_snake_case)]
-fn Tp(satelite:&Phymqp) -> Vec3::Vec3<f64> {
-    return satelite.v*satelite.m;
+fn Tp(satelite:&PhyQty::mxvr) -> Vec3::Vec3<f64> {
+    // println!("{:?}", (*satelite).get_mass());
+    return (*satelite).get_v()*(*satelite).get_mass();
 }
 
 #[allow(dead_code)]
 #[allow(non_snake_case)]
-fn Vq(satelite:&Phymqp, obj:&Vec<Phymqp>) -> Vec3::Vec3<f64> {
+fn Vq(satelite:&PhyQty::mxvr, obj:&Vec<PhyQty::mxvr>) -> Vec3::Vec3<f64> {
     let mut ret:Vec3::Vec3<f64> = Vec3::Vec3([0.0, 0.0, 0.0]);
 
     for iter in obj {
-        ret += iter.r*(PHY_G*satelite.m/iter.r.distance2());
+        println!("{:?}", iter.get_x().distance2());
+        ret += iter.get_x()*(iter.get_mass()/iter.get_x().distance2());
     }
-
-    return ret;
+    // println!("{:?}", ret);
+    return ret*PHY_G*satelite.get_mass();
 }
 
 #[allow(dead_code)]
-fn sympletic4(h:f64, start:f64, end:f64, satelite:&mut Phymqp, obj:&mut Vec<Phymqp>, recoder: &mut Vec<[f32; 10]>) {
+fn sympletic4(h:f64, start:f64, end:f64, satelite:&mut PhyQty::mxvr, obj:&mut Vec<PhyQty::mxvr>, recoder: &mut Vec<[f32; 10]>) {
 
     let c: &Vec<f64> = INSTANCE_C.get().unwrap();
     let d: &Vec<f64> = INSTANCE_C.get().unwrap();
@@ -94,13 +90,17 @@ fn sympletic4(h:f64, start:f64, end:f64, satelite:&mut Phymqp, obj:&mut Vec<Phym
         return;
     }
 
-    while _t < end {
-        for iter in 0..4 {
-            (*satelite).r += Tp(&satelite)*(*c)[iter]*h;
-            (*satelite).v -= Vq(&satelite, &obj)*(*d)[iter]*h;
-        }
+    // let x = satelite.get_x_ptr();
+    // let v = satelite.get_v_ptr();
 
-        record_orbit(_t , 3600.0,satelite, obj, recoder);
+    while _t < end {
+        update_orbit(_t, obj);
+        for iter in 0..4 {
+            satelite.x = satelite.get_x() + Tp(&satelite)*(*c)[iter]*h;
+            satelite.v = satelite.get_v() - Vq(&satelite, &obj)*(*d)[iter]*h;
+            // println!("{:?}", satelite.get_x());
+        }
+        record_orbit(_t , 60.0,satelite, obj, recoder);
 
         _t += h;
     }
@@ -141,41 +141,27 @@ fn main() {
     // let mut _pos :[f64;3] = [0.0, 0.0, 0.0];
     // let mut data:Vec<[f64;4]> = Vec::with_capacity(1000000);
 
-
-    let zeros: Vec3::Vec3<f64> = Default::default();
-    let mut satelite: Phymqp = Phymqp {
+    let mut satelite: PhyQty::mxvr = PhyQty::mxvr {
         name: String::from("SATELITE"),
         m: 50.0,
-        r: zeros.clone(),
-        v: zeros.clone(),
-        rudius:0.25
+        x: Vec3::Vec3([7000.0*1000.0, 0.0, 0.0]),
+        v: Vec3::Vec3([0.0, 3000.0, 0.0]),
+        r:0.25
     };
-    let mut objects:Vec<Phymqp> = Vec::new();
-    objects.push(Phymqp { name: String::from("EARTH"), m:       0.07346*f64::powf(10.0, 24.0), r: zeros.clone(), v: zeros.clone(), rudius:  6371.0*1000.0 });
-    objects.push(Phymqp { name: String::from("MOON") , m:       5.9724 *f64::powf(10.0, 24.0), r: zeros.clone(), v: zeros.clone(), rudius:  1737.4*1000.0 });
-    objects.push(Phymqp { name: String::from("SUN")  , m: 1988500.0    *f64::powf(10.0, 24.0), r: zeros.clone(), v: zeros.clone(), rudius:695700.0*1000.0 });
+    let mut objects:Vec<PhyQty::mxvr> = Vec::new();
+    objects.push(PhyQty::mxvr { name: String::from("EARTH"), m:       0.07346*f64::powf(10.0, 24.0), x: Vec3::Vec3::new(), v: Vec3::Vec3::new(), r:  6371.0*1000.0 });
+    objects.push(PhyQty::mxvr { name: String::from("MOON") , m:       5.9724 *f64::powf(10.0, 24.0), x: Vec3::Vec3::new(), v: Vec3::Vec3::new(), r:  1737.4*1000.0 });
+    objects.push(PhyQty::mxvr { name: String::from("SUN")  , m: 1988500.0    *f64::powf(10.0, 24.0), x: Vec3::Vec3::new(), v: Vec3::Vec3::new(), r:695700.0*1000.0 });
     let mut recorder: Vec<[f32; 10]> = Vec::with_capacity(100000);
 
     cspice::call_furnsh_c();
 
-    sympletic4(1.0, 0.0, 1000000.0, &mut satelite, &mut objects, &mut recorder);
-    
-    // for iter in 0..1000000 {
-    //     cspice::call_splpos_c_moon_earth_j2000((iter) as f64, &mut pos);
-    //     // cspice::call_splpos_c_sun_earth_j2000((iter*3600) as f64, &mut pos);
-    //     // if (pos[0] - _pos[0]).powf(2.0) + (pos[1] - _pos[1]).powf(2.0) + (pos[2] - _pos[2]).powf(2.0) > 10000.0 {
-    //     if iter%3600 == 0 {
-    //         data.push([iter as f64, pos[0], pos[1], pos[2]]);
-    //         _pos.clone_from(&pos);
-    //     }
-    // }
+    sympletic4(1.0, 0.0, 10.0, &mut satelite, &mut objects, &mut recorder);
+
 
     if let Err(err) = csv_writer(&recorder) {
-        println!("error running example: {}", err);
+        println!("{}", err);
         std::process::exit(1);
     }
 
-
-
-    // println!("{} {} {}", pos[0], pos[1], pos[2]);
 }
