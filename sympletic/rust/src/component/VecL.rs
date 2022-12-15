6ae804b8318f0677ce09ea6af0c5906fec5ec4be
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-15 21:16:19 +0900

    ass new() & deref, index operator

--- sympletic/rust/src/component/VecL.rs
+++ sympletic/rust/src/component/VecL.rs
@@ -1,13 +1,37 @@
 pub mod mod_vec_n {
     #[derive(Copy, Clone)]
     pub struct VecL<T, const N: usize>([T; N]);
+
+    #[allow(dead_code)]
+    impl<T, const N: usize> VecL<T, N> {
+        pub fn new() -> Self where T: Default + Copy {
+            return Self([Default::default(); N]);
+        }
+
+        pub fn iter(&self) -> std::slice::Iter<'_, T> {
+            return self.0.iter();
+        }
+    }
+
+    impl<T, const N: usize> std::ops::Deref for VecL<T, N> {
+        type Target = [T; N];
+        fn deref(&self) -> &Self::Target {
+            return & self.0
+        }
+    }
+
+    impl<T, const N: usize> std::ops::DerefMut for VecL<T, N> {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
     
     impl<T, const N: usize> std::ops::Add<VecL<T, N>> for VecL<T, N> where T: std::ops::Add<Output = T> + Copy + Default {
         type Output = VecL<T, N>;
         fn add(self, rhs: Self) -> Self::Output {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] + rhs.0[x];
+                *y = self[x] + rhs[x];
             }
             return Self(tmp);
         }
@@ -17,7 +41,7 @@ pub mod mod_vec_n {
         fn add_assign(&mut self, rhs: Self) {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] + rhs.0[x];
+                *y = self[x] + rhs[x];
             }
             *self = Self(tmp);
         }
@@ -28,7 +52,7 @@ pub mod mod_vec_n {
         fn sub(self, rhs: Self) -> Self::Output {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] - rhs.0[x];
+                *y = self[x] - rhs[x];
             }
             return Self(tmp);
         }
@@ -38,7 +62,7 @@ pub mod mod_vec_n {
         fn sub_assign(&mut self, rhs: Self) {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] - rhs.0[x];
+                *y = self[x] - rhs[x];
             }
             *self = Self(tmp);
         }
@@ -50,7 +74,7 @@ pub mod mod_vec_n {
         fn neg(self) -> Self::Output {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = -self.0[x];
+                *y = -self[x];
             }
             return Self(tmp);
         }
@@ -59,9 +83,9 @@ pub mod mod_vec_n {
     // impl<T, const N: usize> std::ops::Mul<VecL<T, N>> for T where T: std::ops::Mul<Output = T> {
     //     type Output = VecL<T, N>;
     //     fn mul(self, rhs: T) -> Self::Output {
-    //         let mut tmp: Vec<T> = Vec::with_capacity(self.0.len());
+    //         let mut tmp: Vec<T> = Vec::with_capacity(self.len());
 
-    //         let mut iter = self.0.iter();
+    //         let mut iter = self.iter();
     //         for x in iter {
     //             tmp.push(*x * rhs);
     //         }
@@ -74,7 +98,7 @@ pub mod mod_vec_n {
         fn mul(self, rhs: A) -> Self::Output {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] * rhs;
+                *y = self[x] * rhs;
             }
             return Self(tmp);
         }
@@ -84,7 +108,7 @@ pub mod mod_vec_n {
         fn mul_assign(&mut self, rhs: A) {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] * rhs;
+                *y = self[x] * rhs;
             }
             *self = Self(tmp);
         }
@@ -95,9 +119,24 @@ pub mod mod_vec_n {
         fn div(self, rhs: A) -> Self::Output {
             let mut tmp: [T; N] = [Default::default(); N];
             for (x, y) in tmp.iter_mut().enumerate() {
-                *y = self.0[x] / rhs;
+                *y = self[x] / rhs;
             }
             return Self(tmp);
+        }
+    }
+
+    impl<T, const N: usize> std::ops::Index<usize> for VecL<T, N> {
+        type Output = T;
+    
+        fn index(&self, _index: usize) -> &Self::Output {
+            return &((*self).0[_index]);
+        }
+    }
+
+    impl<T, const N: usize> std::ops::IndexMut<usize> for VecL<T, N> {
+    
+        fn index_mut(&mut self, _index: usize) -> &mut Self::Output {
+            return &mut ((*self).0[_index]);
         }
     }
 }
