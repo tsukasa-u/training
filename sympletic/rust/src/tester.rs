0f410b4e1b934f4131e67b7664e9680cf97d1ffa
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-27 02:43:38 +0900

    Update tester.rs

---
+++ sympletic/rust/src/tester.rs
@@ -0,0 +1,8 @@
+
+// use gmp_mpfr_sys::mpfr::mpfr_t;
+use crate::component::interval::interval;
+
+pub fn tester() {
+    let mut x: interval::Interval = interval::Interval::new();
+    x.init();
+}
