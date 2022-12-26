d6a5853d633a646e818aff663abe426b40b26162
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-27 02:44:45 +0900

    add mpfi library

--- sympletic/rust/build.rs
+++ sympletic/rust/build.rs
@@ -7,4 +7,13 @@ fn main(){
     println!("cargo:rustc-link-search={}/cspice/lib/", project_dir);
     println!("cargo:rustc-link-lib=static=cspice");
 
+    // println!("cargo:rustc-link-search={}/mpfi-1.5.4/src/.libs", project_dir);
+    // println!("cargo:rustc-link-lib=init");
+    
+    // println!("cargo:rustc-link-search={}/mpfi-1.5.4/src", project_dir);
+    // println!("cargo:rustc-link-lib=init");
+    
+    println!("cargo:rustc-link-search=/usr/local/lib");
+    println!("cargo:rustc-link-lib=mpfi")
+    
 }
