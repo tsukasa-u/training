0dcb400ebcc36dc0876199d36088312d325d823b
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-31 16:39:05 +0900

    Update build.rs

--- sympletic/rust/build.rs
+++ sympletic/rust/build.rs
@@ -6,14 +6,9 @@ fn main(){
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:rustc-link-search={}/cspice/lib/", project_dir);
     println!("cargo:rustc-link-lib=static=cspice");
-
-    // println!("cargo:rustc-link-search={}/mpfi-1.5.4/src/.libs", project_dir);
-    // println!("cargo:rustc-link-lib=init");
-    
-    // println!("cargo:rustc-link-search={}/mpfi-1.5.4/src", project_dir);
-    // println!("cargo:rustc-link-lib=init");
     
     println!("cargo:rustc-link-search=/usr/local/lib");
-    println!("cargo:rustc-link-lib=mpfi")
+    println!("cargo:rustc-link-lib=mpfi");
+    println!("cargo:rustc-link-lib=mpfr");
     
 }
