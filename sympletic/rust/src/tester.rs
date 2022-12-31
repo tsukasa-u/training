1935a487ce29090c3072a5611c0550b64fb8aab3
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-31 13:35:22 +0900

    Update tester.rs

--- sympletic/rust/src/tester.rs
+++ sympletic/rust/src/tester.rs
@@ -6,6 +6,6 @@ pub fn tester() {
     let mut x: interval::mpfi_t = interval::mpfi_t::new();
     x.init();
     x.set_si(3);
-    let y: interval::mpfi_t = 1 as i64 + x;
+    let y: interval::mpfi_t = 1 + x;
     println!("{}", y.get_d())
 }
