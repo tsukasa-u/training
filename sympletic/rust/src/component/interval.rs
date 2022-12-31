b1560f24887a67ef6462c150419ac0e2c3d3acc2
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2022-12-31 13:35:31 +0900

    Update interval.rs

--- sympletic/rust/src/component/interval.rs
+++ sympletic/rust/src/component/interval.rs
@@ -1,12 +1,12 @@
 
-#[cfg(target_os="linux")]
+// #[cfg(target_os="linux")]
 pub mod interval;
 
-#[cfg(target_os="linux")]
+// #[cfg(target_os="linux")]
 mod gmp;
 
-#[cfg(target_os="linux")]
+// #[cfg(target_os="linux")]
 mod mpfr;
 
-#[cfg(target_os="linux")]
+// #[cfg(target_os="linux")]
 mod mpfi;
