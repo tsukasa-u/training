ef0fe9ff0838aca75a922e223e7442ca52805e50
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-06-22 02:23:43 +0900

    Create importPkg.jl

---
+++ JUMP/julia/sample8/importPkg.jl
@@ -0,0 +1,14 @@
+import Pkg
+
+Pkg.add("JuMP")
+Pkg.add("Ipopt")
+# Pkg.add(" NLopt")
+Pkg.add("Plots")
+
+Pkg.add("Polynomials")
+Pkg.add("SpecialPolynomials")
+Pkg.add("SpecialFunctions")
+
+Pkg.add("ForwardDiff")
+
+Pkg.add("Roots")
