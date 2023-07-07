641ce0b89e49e3e9d3129269fe62011d45e68d66
Oguri Hideyuki <ogu.hide.u@gmail.com>
2023-07-07 18:13:28 +0900

    Update untitled1.jl

--- DDP/sample1/untitled1.jl
+++ DDP/sample1/untitled1.jl
@@ -1,54 +1,103 @@
 using LinearAlgebra
 using ForwardDiff
+using Plots
+using CPUTime
 
-n = 10
+f(x, u) = begin
+    r, v_r, v_t = x
+    u_rp, u_rm, u_tp, u_tm = u
+    return [
+        (v_r),
+        (v_t^2.0/r - 1.0/r^2.0 + u_rp - u_rm),
+        (-v_r*v_t/r + u_tp - u_tm)
+    ]
+end
+fx(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(x -> f(x, u), x)
+fu(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(u -> f(x, u), u)
+fxx(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(x -> ForwardDiff.jacobian(x -> f(x, u), x), x)
+fux(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(x -> ForwardDiff.jacobian(u -> f(x, u), u), x)
+fuu(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(u -> ForwardDiff.jacobian(u -> f(x, u), u), u)
 
-f(x, u) = x
-fx(x, u) = ForwardDiff.gradient(x -> f(x, u), x)
-fu(x, u) = ForwardDiff.gradient(x -> f(x, u), u)
-fxx(x, u) = ForwardDiff.jacobian(x -> fx(x, u), x)
-fux(x, u) = ForwardDiff.jacobian(u -> fx(x, u), u)
-fuu(x, u) = ForwardDiff.jacobian(u -> fu(x, u), u)
+l(x, u) = begin
+    r, v_r, v_t = x
+    u_rp, u_rm, u_tp, u_tm = u
+    return u_rp + u_rm + u_tp + u_tm + (u_rp*u_rm + u_tp*u_tm)
+end
+lx(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.gradient(x -> l(x, u), x)
+lu(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.gradient(u -> l(x, u), u)
+lxx(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.hessian(x -> l(x, u), x)
+lux(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.jacobian(x -> ForwardDiff.gradient(u -> l(x, u), u), x)
+luu(x::Vector{Real}, u::Vector{Real}) = ForwardDiff.hessian(u -> l(x, u), u)
 
-l(x, u) = 1.0
-lx(x, u) = ForwardDiff.gradient(x -> l(x, u), x)
-lu(x, u) = ForwardDiff.gradient(u -> l(x, u), u)
-lxx(x, u) = ForwardDiff.jacobian(x -> lx(x, u), x)
-lux(x, u) = ForwardDiff.jacobian(u -> lx(x, u), u)
-luu(x, u) = ForwardDiff.jacobian(u -> lu(x, u), u)
+lf(x) = sum((x .- [4.0, 0.0, 0.5]).^2.0)
+# lf(x) = sum((x .- [4.0, 0.0, 0.5]).^2.0)/sum(x.^2.0)
 
-lf(x) = 0.0
+# v(i, x, u) = sum(l(_x, _u) for (_x, _u) in zip(x[i:end], u[i:end]); init = 0.0) + lf(x[end])
+v(i, x, u, n) = begin
+    if i == n
+        return l(x, u[i]) + lf(x)
+    else
+        return l(x, u[i]) + v(i + 1, f(x, u[i]), u, n)
+    end
+end
+vx(i, x::Vector{Real}, u::Vector{Vector{Real}}, n) = ForwardDiff.gradient(x -> v(i, x, u, n), x)
+vxx(i, x::Vector{Real}, u::Vector{Vector{Real}}, n) = ForwardDiff.hessian(x -> v(i, x, u, n), x)
 
-v(i, x, u) = sum(l(_x, _u) for (_x, _u) in zip(x[i:end], u[i:end])) + lf(x[end])
-vx(i, x, u) = ForwardDiff.gradient(x -> v(i, x, u), x)
-vxx(i, x, u) = ForwardDiff.jacobian(x -> vx(i, x, u), x)
+function compute_Q(idx, x, u; __vxx = [;], __vx = [;])
 
-function compute_Q(i, x, u)
+    # println(__vxx)
+    # println(__vx)
 
-    _lx = lx(x, u)
-    _lu = lu(x, u)
-    _lxx = lxx(x, u)
-    _lux = lux(x, u)
-    _luu = luu(x, u)
-    _vx = vx(i + 1, x, u)
-    _vxx = vxx(i + 1, x, u)
-    _fx = fx(x, u)
-    _fu = fu(x, u)
-    _fxx = fxx(x, u)
-    _fux = fux(x, u)
-    _fuu = fuu(x, u)
+    nx = size(x[1], 1)
+    nu = size(u[1], 1)
+    n = size(x, 1)
+
+    _lx = lx(x[idx], u[idx])
+    _lu = lu(x[idx], u[idx])
+    _lxx = lxx(x[idx], u[idx])
+    _lux = lux(x[idx], u[idx])
+    _luu = luu(x[idx], u[idx])
+
+    # println(isassigned(__vxx))
+    # println(isempty(__vxx))
+    # println(size(__vx))
+    if isassigned(__vxx) && isassigned(__vx)
+        _vx = __vx
+        _vxx = __vxx
+    else
+        _vx = vx(idx + 1, x[idx], u, n)
+        _vxx = vxx(idx + 1, x[idx], u, n)
+    end
+
+    _fx = fx(x[idx], u[idx])
+    _fu = fu(x[idx], u[idx])
+    _fxx = reshape(fxx(x[idx], u[idx]), (nx, nx, nx))
+    _fux = reshape(fux(x[idx], u[idx]), (nx, nu, nx))
+    _fuu = reshape(fuu(x[idx], u[idx]), (nx, nu, nu))
 
     Qx = _lx + transpose(_fx) * _vx
     Qu = _lu + transpose(_fu) * _vx
-    Qxx = _lxx + transpose(_fx) * _vxx * _fx + kron(_vx, _fxx)
-    Qux = _lux + transpose(_fu) * _vxx * _fx + kron(_vx, _fux)
-    Quu = _luu + transpose(_fu) * _vxx * _fu + kron(_vx, _fuu)
+
+    Qxx = _lxx + transpose(_fx) * _vxx * _fx + sum(_vx[i] * _fxx[i, :, :] for i in 1:nx)
+    Qux = _lux + transpose(_fu) * _vxx * _fx + sum(_vx[i] * _fux[i, :, :] for i in 1:nx)
+    Quu = _luu + transpose(_fu) * _vxx * _fu + sum(_vx[i] * _fuu[i, :, :] for i in 1:nx)
+
+    # println(Qx)
+    # println(Qu)
+    # println(Qxx)
+    # println(Qux)
+    # println(Quu)
+
+    # Qxx = _lxx + transpose(_fx) * _vxx * _fx + kron(_vx, _fxx)
+    # Qux = _lux + transpose(_fu) * _vxx * _fx + kron(_vx, _fux)
+    # Quu = _luu + transpose(_fu) * _vxx * _fu + kron(_vx, _fuu)
 
     return Qx, Qu, Qxx, Qux, Quu
 end
 
-function normalize_inverse_Quu(Quu)
-    return inv(normalize(Quu, 1e-6))
+function inverse_Quu(Quu)
+    @assert det(Quu) != 0.0 "non-regular matrix"
+    return inv(Quu)
 end
 
 function compute_k(Qxx_1, Qu)
@@ -59,39 +108,62 @@ function compute_K(Quu_1, Qux)
     return -Quu_1 * Qux
 end
 
-function BFP(n, x, u)
-
-    k = []
-    K = []
-
-    for i in (n-1):-1:1
-        Qx, Qu, Qxx, Qux, Quu = compute_Q(i, x, u)
-        Quu_1 = normalize_inverse_Quu(Quu)
-        push!(k, compute_k(Quu_1, Qu))
-        push!(K, compute_K(Quu_1, Qux))
-    end
-
-    return k, K
+function compute_Vx(Qx, Qu, K)
+    return Qx + transpose(K) * Qu
 end
 
-function FFP(n, x, u, α)
+function compute_Vxx(Qxx, Qux, K)
+    return Qxx + transpose(K) * Qux
+end
+
+function BFP(n, x, u)
+
+    list_k = []
+    list_K = []
+    
+    Qx, Qu, Qxx, Qux, Quu = compute_Q(n - 1, x, u)
+    Quu_1 = inverse_Quu(Quu)
+    k = compute_k(Quu_1, Qu)
+    K = compute_K(Quu_1, Qux)
+    push!(list_k, k)
+    push!(list_K, K)
+
+    for i in (n - 2):-1:1
+        Vxx = compute_Vxx(Qxx, Qux, K)
+        Vx = compute_Vx(Qx, Qu, K)
+        Qx, Qu, Qxx, Qux, Quu = compute_Q(i, x, u; __vxx=Vxx, __vx = Vx)
+        Quu_1 = inverse_Quu(Quu)
+        k = compute_k(Quu_1, Qu)
+        K = compute_K(Quu_1, Qux)
+
+        push!(list_k, k)
+        push!(list_K, K)
+    end
+
+    return list_k, list_K
+end
+
+function FFP(n, x, u, α, dt, k, K)
 
     _x = copy(x)
     _u = copy(u)
     for i in 1:(n-1)
-        _u[i] = u[i] + α*k[i] + K[i] * (_x[i] - x[i])
-        _x[i + 1] = f(_x[i], _u[i])
+        # println(u[i])
+        # println(α*k[i])
+        # println(K[i] * (_x[i] - x[i]))
+        _u[i] = u[i] .+ α*k[i] .+ K[i] * (_x[i] - x[i])
+        _x[i + 1] = _x[i] + f(_x[i], _u[i])*dt
     end
     
     return _x, _u
 end
 
-function BFFP(n, x, u, α)
+function BFFP(n, x, u, α, dt)
 
-    BFP(n, x, u)
-    _x, u = FFP(n, x, u, α)
+    k, K = BFP(n, x, u)
+    _x, u = FFP(n, x, u, α, dt, k, K)
 
-    ret = max(abs.(_x. - x)) < 1e-6
+    ret = maximum(maximum(abs.(_x[i] - x[i]) for i in 1:n)) < 1e-6
 
     x = copy(_x)
 
@@ -99,14 +171,49 @@ function BFFP(n, x, u, α)
 end
 
 function loop()
+    n = 101
+    t0 = 0
+    tf = 55
+    dt = (tf - t0)/(n - 1)
+
+    nx = 3
+    nu = 4
+
+    x0 = [1.0, 0.0, 1.0]
+    xf = [4.0, 0.0, 0.5]
     
-        x = [0.0 for i in 1:n]
-        u = [0.0 for i in 1:n]
-    
-        for i in 1:100
-            BFFP(n, x, u, 0.8)
-            println(x)
+    x = [Real[(xf[j] - x0[j])*i/(n - 1) + x0[j] for j in 1:nx] for i in 1:n]
+    u = [Real[0.001 for j in 1:nu] for i in 1:n]
+
+    for idx in 1:1
+        ret = BFFP(n, x, u, 0.8, dt)
+        # println(x)
+        plot_graph(idx, [x[i][j] for j in 1:nx, i in 1:n], [u[i][j] for j in 1:nu, i in 1:n], [dt*i for i in 1:n])
+        if ret
+            break
         end
+    end
 end
 
-loop()
+
+function plot_graph(index, plot_x, plot_u, plot_t)
+
+    plots_x = Plots.plot(
+        plot_t[:], 
+        [ plot_x[1, :] plot_x[2, :] plot_x[3, :] ], 
+        label=["r" "v_r" "v_t"],
+        xlabel = "t",
+        st=:scatter,
+    )
+
+    plots_u = Plots.plot(
+        plot_t, 
+        [ plot_u[1, :].-plot_u[2, :] plot_u[3, :].-plot_u[4, :] ], 
+        label=["u_r" "u_t"],
+        xlabel = "t",
+        st=:scatter
+    )
+    png(string(index, base = 10, pad = 2))
+end
+
+@time @CPUtime loop()
