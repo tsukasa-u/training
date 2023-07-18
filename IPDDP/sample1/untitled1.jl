72f8dbfbd8f80069b7724048780c016ee0e56356
Oguri Hideyuki <ogu.hide.u@gmail.com>
2023-07-18 12:04:41 +0900

    Update untitled1.jl

--- IPDDP/sample1/untitled1.jl
+++ IPDDP/sample1/untitled1.jl
@@ -27,12 +27,13 @@ f(x, u) = begin
         (v_r),
         (v_t^2.0/r - 1.0/r^2.0 + u_rp - u_rm),
         (-v_r*v_t/r + u_tp - u_tm)
-    ]
+    ]*0.01 + x
 end
 fx(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(x -> f(x, u), x)
 fu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(u -> f(x, u), u)
 fxx(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(x -> ForwardDiff.jacobian(x -> f(x, u), x), x)
 fux(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(x -> ForwardDiff.jacobian(u -> f(x, u), u), x)
+# fxu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(u -> ForwardDiff.jacobian(x -> f(x, u), x), u)
 fuu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(u -> ForwardDiff.jacobian(u -> f(x, u), u), u)
 
 l(x, u) = begin
@@ -40,10 +41,11 @@ l(x, u) = begin
     u_rp, u_rm, u_tp, u_tm = u
     return u_rp + u_rm + u_tp + u_tm + (u_rp*u_rm + u_tp*u_tm)
 end
-lx(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.gradient(x -> l(x, u), x,)
+lx(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.gradient(x -> l(x, u), x)
 lu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.gradient(u -> l(x, u), u)
 lxx(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.hessian(x -> l(x, u), x)
 lux(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(x -> ForwardDiff.gradient(u -> l(x, u), u), x)
+# lxu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.jacobian(u -> ForwardDiff.gradient(x -> l(x, u), x), u)
 luu(x::Vector{T}, u::Vector{T}) where T<:Real = ForwardDiff.hessian(u -> l(x, u), u)
 
 lf(x) = sum((x .- [4.0, 0.0, 0.5]).^2.0)
@@ -109,10 +111,10 @@ mutable struct tuple_w{T<:Real, S<:Integer}
         self.nu = _nu
         self.ns = _ns
 
-        self.x = zeros(_nx)
+        self.x = ones(_nx)
         self.u = zeros(_nu)
-        self.s = zeros(_ns)
-        self.y = zeros(_ns)
+        self.s = ones(_ns)
+        self.y = ones(_ns)
 
         return self
     end
@@ -161,9 +163,9 @@ mutable struct struct_coefficients{T<:Real, S<:Integer}
         self.η = zeros(_ns)
         self.χ = zeros(_ns)
 
-        self.β = zeros(_nx, _nu)
-        self.θ = zeros(_nx, _ns)
-        self.ζ = zeros(_nx, _ns)
+        self.β = zeros(_nu, _nx)
+        self.θ = zeros(_ns, _nx)
+        self.ζ = zeros(_ns, _nx)
 
         return self
     end
@@ -188,11 +190,11 @@ function init_Q!(_Q::struct_Q, w::tuple_w)
     _vNxx = vNxx(w.x)
 
     _Q.Qx[:] .= lx(w.x, w.u) + fx_T*_vNx
-    _Q.Qx[:] .= lu(w.x, w.u) + fu_T*_vNx
-
+    _Q.Qu[:] .= lu(w.x, w.u) + fu_T*_vNx
 
     _Q.Qxx[:, :] .= lxx(w.x, w.u) + fx_T*_vNxx*_fx + sum(_vNx[i] * _fxx[i, :, :] for i in 1:_Q.nx)
-    _Q.Qxu[:, :] .= lxu(w.x, w.u) + fx_T*_vNxx*_fu + sum(_vNx[i] * _fxu[i, :, :] for i in 1:_Q.nx)
+    # _Q.Qxu[:, :] .= lxu(w.x, w.u) + fx_T*_vNxx*_fu + sum(_vNx[i] * _fxu[i, :, :] for i in 1:_Q.nx)
+    _Q.Qxu[:, :] .= transpose(lux(w.x, w.u)) + fx_T*_vNxx*_fu + sum(_vNx[i] * transpose(_fux[i, :, :]) for i in 1:_Q.nx)
     _Q.Quu[:, :] .= luu(w.x, w.u) + fu_T*_vNxx*_fu + sum(_vNx[i] * _fuu[i, :, :] for i in 1:_Q.nx)
 end
 
@@ -206,66 +208,83 @@ function compute_coeff!(coeff::struct_coefficients, w::tuple_w, r::tuple_r, Q::s
     I = ones(w.ns, w.ns)
     zero_us = zeros(w.nu, w.ns)
     zero_su = zeros(w.ns, w.nu)
-    zero_sx = zeros(w.ns, w.nu)
+    zero_ss = zeros(w.ns, w.ns)
+    zero_sx = zeros(w.ns, w.nx)
 
-    r.rp .= c(w.x, w.u) + w.y
-    r.rd .= S*w.y - μ
+    r.rp[:] .= c(w.x, w.u) + w.y
+    r.rd[:] .= S*w.y - μ
+    r.rhat[:] .= S*r.rp - r.rd
 
-    P = [Q.Quu Q.Qus zero_us; Q.Qsu Q.Qss zero_su I; zero_us Y S]
+    # P = [Q.Quu transpose(Q.Qsu) zero_us; Q.Qsu zero_ss I; zero_su Y S]
 
-    ret = inv(P)*[Q.Qu Q.Qux; rp Q.Qsx; rd zero_sx]
+    # ret = -inv(P)*[Q.Qu Q.Qux; rp Q.Qsx; rd zero_sx]
 
-    coeff.α .= vec(ret[1:w.nx, 1])
-    coeff.η .= vec(ret[(w.nx + 1):(w.nx + w.nu), 1])
-    coeff.χ .= vec(ret[(w.nx + w.nu + 1):end, 1])
+    # coeff.α .= vec(ret[1:w.nx, 1])
+    # coeff.η .= vec(ret[(w.nx + 1):(w.nx + w.nu), 1])
+    # coeff.χ .= vec(ret[(w.nx + w.nu + 1):end, 1])
 
-    coeff.β .= ret[1:w.nx, 2:end]
-    coeff.θ .= ret[(w.nx + 1):(w.nx + w.nu), 2:end]
-    coeff.ζ .= ret[(w.nx + w.nu + 1):end, 2:end]
+    # coeff.β .= ret[1:w.nx, 2:end]
+    # coeff.θ .= ret[(w.nx + 1):(w.nx + w.nu), 2:end]
+    # coeff.ζ .= ret[(w.nx + w.nu + 1):end, 2:end]
+
+    SY_1 = S*inv(Y)
+    Y_1 = inv(Y)
+
+    R = (Q.Quu + transpose(Q.Qsu)*SY_1*Q.Qsu)
+    ret = -inv(R)*[Q.Qu + transpose(Q.Qsu)*Y_1*r.rhat transpose(Q.Qxu) + transpose(Q.Qsu)*SY_1*Q.Qsx]
+    
+    coeff.α[:] .= vec(ret[:, 1])
+
+    coeff.β[:, :] .= ret[:, 2:end]
+
+    coeff.η[:] .= Y_1*(r.rhat + S*Q.Qsu*coeff.α)
+    coeff.θ[:, :] .= SY_1*(Q.Qsx + Q.Qsu*coeff.β)
+
+    coeff.χ[:] .= -r.rp - Q.Qsu*coeff.α
+    coeff.ζ[:, :] .= -Q.Qsx - Q.Qsu*coeff.β
 
 end
 
 function update_Q!(Q::struct_Q, w::tuple_w, r::tuple_r)
     Y_1 = inv(diagm(w.y))
     S = diagm(w.s)
-    r.rhat .= S*r.rp - r.rd
 
     Qsx_T = transpose(Q.Qsx)
     Qsu_T = transpose(Q.Qsu)
 
-    Q.Qx .+= Qsx_T*Y_1*r.rhat
-    Q.Qu .+= Qsu_T*Y_1*r.rhat
-    Q.Qxx .+= Qsx_T*Y_1*S*Q.Qsx
-    Q.Qxu .+= Qsx_T*Y_1*S*Q.Qsu
-    Q.Quu .+= Qsu_T*Y_1*S*Q.Qsu
+    Q.Qx[:] .+= Qsx_T*Y_1*r.rhat
+    Q.Qu[:] .+= Qsu_T*Y_1*r.rhat
+    Q.Qxx[:, :] .+= Qsx_T*Y_1*S*Q.Qsx
+    Q.Qxu[:, :] .+= Qsx_T*Y_1*S*Q.Qsu
+    Q.Quu[:, :] .+= Qsu_T*Y_1*S*Q.Qsu
 
-    _Q.Qs .= c(w.x, w.u)
-    _Q.Qsx .= cx(w.x, w.u)
-    _Q.Qsu .= cu(w.x, w.u)
-    _Q.Qss .= zeros(Q.ns, Q.ns)
+    Q.Qs[:] .= c(w.x, w.u)
+    Q.Qsx[:, :] .= cx(w.x, w.u)
+    Q.Qsu[:, :] .= cu(w.x, w.u)
+    Q.Qss[:, :] .= zeros(Q.ns, Q.ns)
 end
 
-function FFP!(nw::S, list_w::Array{tuple_w, 1}, list_coeff::Array{struct_coefficients, 1}) where S<:Integer
+function FFP!(nw::S, list_w::Array{tuple_w{T, S}, 1}, list_coeff::Array{struct_coefficients{T, S}, 1}) where {S<:Integer, T<:Real}
     x = list_w[1].x
     u = zeros(list_w[1].nu)
     s = zeros(list_w[1].ns)
     y = zeros(list_w[1].ns)
     for i in 1:nw
-        u .= φ(x, w, list_coeff[i])
-        s .= ψ(x, w, list_coeff[i])
-        y .= ξ(x, w, list_coeff[i])
+        u .= φ(x, list_w[i], list_coeff[i])
+        s .= ψ(x, list_w[i], list_coeff[i])
+        y .= ξ(x, list_w[i], list_coeff[i])
 
-        list_w[i].u .= u
-        list_w[i].s .= s
-        list_w[i].y .= y
-        list_w[i].x .= x
+        list_w[i].u[:] .= u
+        list_w[i].s[:] .= s
+        list_w[i].y[:] .= y
+        list_w[i].x[:] .= x
 
         x .= f(x, u)
     end
     
 end
 
-function BFP!(nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S}, 1}, list_coeff::Array{struct_coefficients{T, S}, 1}, μ::Vector{T}) where{S<:Integer, T<:Real}
+function BFP!(nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S}, 1}, list_coeff::Array{struct_coefficients{T, S}, 1}, μ::Vector{T}) where {S<:Integer, T<:Real}
     
     Q = struct_Q(list_w[1].nx, list_w[1].nu, list_w[1].ns, Float64)
     init_Q!(Q, list_w[1])
@@ -276,15 +295,27 @@ function BFP!(nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S
     end
 end
 
-function loop!(n::S, nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S}, 1}, list_coeff::Array{struct_coefficients{T, S}, 1}, μ::Vector{T}) where{S<:Integer, T<:Real}
-    for _ in 1:n
+# function check_constraints(nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S}, 1})
+
+# end
+
+function loop!(n::S, nw::S, list_w::Array{tuple_w{T, S}, 1}, list_r::Array{tuple_r{T, S}, 1}, list_coeff::Array{struct_coefficients{T, S}, 1}, μ::Vector{T}) where {S<:Integer, T<:Real}
+    for k in 1:n
         BFP!(nw, list_w, list_r, list_coeff, μ)
         FFP!(nw, list_w, list_coeff)
+        plot_graph(
+            k, 
+            [list_w[i].x[j] for j in 1:list_w[1].nx, i in 1:nw],
+            [list_w[i].u[j] for j in 1:list_w[1].nu, i in 1:nw], 
+            [list_w[i].s[j] for j in 1:list_w[1].ns, i in 1:nw], 
+            [list_w[i].y[j] for j in 1:list_w[1].ns, i in 1:nw], 
+            [0.01 * i for i in 1:nw]
+        )
     end
 end
 
 function main()
-    nw = 10
+    nw = 5500
     nx = 3
     nu = 4
     ns = 7
@@ -292,7 +323,55 @@ function main()
     list_r = [tuple_r(ns, Float64) for _ in 1:nw]
     list_coeff = [struct_coefficients(nx, nu, ns, Float64) for _ in 1:nw]
     μ = ones(ns)
-    loop!(100, nw, list_w, list_r, list_coeff, μ)
+    loop!(10, nw, list_w, list_r, list_coeff, μ)
 end
 
-main()
+
+function plot_graph(index, plot_x, plot_u, plot_s, plot_y, plot_t)
+
+    plots_x = Plots.plot(
+        plot_t[:], 
+        [ plot_x[1, :] plot_x[2, :] plot_x[3, :] ], 
+        label=["r" "v_r" "v_t"],
+        xlabel = "t",
+        st=:scatter,
+    )
+
+    plots_u = Plots.plot(
+        plot_t, 
+        [ plot_u[1, :].-plot_u[2, :] plot_u[3, :].-plot_u[4, :] ], 
+        label=["u_r" "u_t"],
+        xlabel = "t",
+        st=:scatter
+    )
+
+    plots_s = Plots.plot(
+        plot_t, 
+        [ plot_s[i, :] for i in 1:7], 
+        label=["s$i" for i in 1:7],
+        xlabel = "t",
+        st=:scatter
+    )
+
+    plots_y = Plots.plot(
+        plot_t, 
+        [ plot_y[i, :] for i in 1:7], 
+        label=["y$i" for i in 1:7],
+        xlabel = "t",
+        st=:scatter
+    )
+    
+    plot_ref = Plots.plot(
+        plots_x,
+        plots_u,
+        plots_s,
+        plots_y,
+        layout = (2, 2),
+        # legend = false,
+        # margin = 1Plots.cm,
+    )
+    png(string(index, base = 10, pad = 2))
+end
+
+
+@time @CPUtime main()
