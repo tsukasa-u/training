0d9b54b3081df4f6b6d647bb7ab6e31f11b52597
Oguri Hideyuki <ogu.hide.u@gmail.com>
2023-10-07 18:41:02 +0900

    Update main.jl

--- HM-iLQR/sample2/main.jl
+++ HM-iLQR/sample2/main.jl
@@ -1,13 +1,15 @@
-
-
 using RobotZoo
 using RobotDynamics
 using LinearAlgebra
 using Plots
 
+include("HM-iLQR.jl")
+include("M-iLQR.jl")
+include("func.jl")
+using .HM_iLQR
+using .M_iLQR
+using .func
 
-# include("HM-iLQR.jl")
-# using .HM_iLQR
 
 function main()
 
@@ -15,8 +17,8 @@ function main()
     n,m = RobotDynamics.dims(model)
 
     funcs = func.FUNC()
-    funcs.a = model
     funcs._h = 0.05
+    funcs.f = (x, u) -> func.dynamics_rk4(x, u, (x, u) -> RobotZoo.dynamics(model, x, u), funcs._h)
 
     Q = Diagonal([1.0*ones(2); 1.0*ones(2)])
     R = 0.1*ones(1, 1)
@@ -25,7 +27,9 @@ function main()
     funcs.l = (x, u) -> 0.5*((x-xgoal)'*Q*(x-xgoal) + u'*R*u)
     funcs.lf = (x) -> 0.5*((x-xgoal)'*Qn*(x-xgoal))
     funcs.ge = (x, u) -> [0.0]
+    funcs.glf = (x) -> [0.0]
     funcs.gl = (x, u) -> [0.0]
+    funcs.glf = (x) -> [0.0]
 
     N = 100
     M = 1
@@ -38,6 +42,8 @@ function main()
     U_init = func.Marray(L, M, N, (m,), 0.001*ones(N,m))
 
     X, U, K = HM_iLQR.RunHM_iLQR(X_init, U_init, N, M, MaxIter, ϵ_v, d_max, funcs)
+
+    return 
 
 end
 
